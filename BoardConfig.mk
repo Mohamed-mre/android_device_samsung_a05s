7b363c664627091730e1c7f176dd34e80dd7ec83
SavedByLight <mrfluffyoven@gmail.com>
2025-11-20 17:12:15 +0000

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,227 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/samsung/a05s
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a9
+
+# Additional
+TARGET_USES_UEFI := true
+TARGET_IS_64_BIT := true
+TARGET_USES_64_BIT_BINDER := true # For android-12.1 and below
+TARGET_BOARD_SUFFIX := _64
+TARGET_NO_BOOTLOADER := true
+
+# Board
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_NO_RADIOIMAGE := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Bootloader
+BOARD_VENDOR := samsung
+TARGET_SOC := bengal
+TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_SOC)
+TARGET_BOARD_PLATFORM := $(TARGET_SOC)
+QCOM_BOARD_PLATFORMS := $(TARGET_SOC)
+TARGET_BOARD_PLATFORM_GPU := Adreno-610
+
+# Display
+TW_THEME := portrait_hdpi
+TARGET_SCREEN_DENSITY := 403
+TARGET_SCREEN_HEIGHT := 1080
+TARGET_SCREEN_WIDTH := 2340
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_MAX_BRIGHTNESS := 500
+TW_DEFAULT_BRIGHTNESS := 255
+TW_FRAMERATE := 120
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+
+# Status Bar UI
+TW_STATUS_ICONS_ALIGN := center
+TW_CUSTOM_CPU_POS := "50"
+TW_CUSTOM_CLOCK_POS := "300"
+TW_CUSTOM_BATTERY_POS := "750"
+
+# Cmdline
+BOARD_KERNEL_CMDLINE :=  \
+	video=vfb:640x400,bpp=32,memsize=3072000 \
+	printk.devkmsg=on \
+	firmware_class.path=/vendor/firmware_mnt/image \
+	onsole=null \
+	bootconfig \
+	androidboot.hardware=qcom \
+	hardware=qcom \
+	androidboot.memcg=1 \
+	androidboot.usbcontroller=a600000.dwc3 \
+	androidboot.init_fatal_panic=true \
+	androidboot.selinux=permissive \
+	loop.max_part=7
+
+# Kernel
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
+
+# Kernel - prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
+BOARD_ROOT_EXTRA_FOLDERS := \
+    carrier \
+    efs \
+    omr \
+    optics \
+    prism \
+    spu \
+    persist \
+    sec_efs \
+    firmware \
+    metadata \
+    keydata \
+    keyrefuge
+
+# MKBOOTIMG
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 100663296
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
+BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system system system vendor system_ext vendor_dlkm product odm
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
+
+# Recovery
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+# File System & Storage
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+TW_SKIP_ADDITIONAL_FSTAB := true
+TW_USE_EXTERNAL_STORAGE := true
+TARGET_USES_MKE2FS := true 
+TW_INCLUDE_NTFS_3G := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# Encryption
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+BOARD_USES_METADATA_PARTITION := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+
+# USB
+TW_USE_NEW_MINADBD := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+
+# Misc
+TW_INCLUDE_REPACKTOOLS := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_LPTOOLS := true
+TW_INCLUDE_LPDUMP := true
+
+# TWRP Configuration
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_APEX := true
+TW_NO_BIND_SYSTEM := true
+TW_PREPARE_DATA_MEDIA_EARLY := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone50/temp"
+TW_CUSTOM_BATTERY_PATH := "/sys/class/power_supply/battery/capacity"
+
+# Samsung
+TW_NO_REBOOT_BOOTLOADER := true
+TW_USE_SAMSUNG_HAPTICS := true
+TW_INCLUDE_FASTBOOTD := true
+TW_HAS_DOWNLOAD_MODE := true
+
+# Copy Out 
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Logging
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+TWRP_EVENT_LOGGING := true
+
+# Properties
+TW_NO_LEGACY_PROPS := true
+TW_INCLUDE_LIBRESETPROP := true
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Treble
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
+PRODUCT_FULL_TREBLE := true
+
+# Skyhawk Recovery - ONLY FOR SKYHAWK RECOVERY PROJECT BUILDING
+SHRP_DARK := true
+SHRP_FLASH_MAX_BRIGHTNESS := 500
+SHRP_DEVICE_CODE := a05s
+SHRP_FLASH := 1
+
+# PBRP- ONLY FOR PITCH BLACK RECOVERY PROJECT BUILDING
+PB_TORCH_PATH := "/sys/devices/virtual/camera/flash/rear_flash"
+PB_TORCH_MAX_BRIGHTNESS := 1
+
+# Version/Maintainer
+TW_DEVICE_VERSION := SavedByLight Pre-Release
+SHRP_MAINTAINER := SavedByLight
+MAINTAINER := SavedByLight
+
+# For testing only
+BETA_BUILD := true
