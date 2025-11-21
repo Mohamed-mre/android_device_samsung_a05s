9829313c8dbc351d5d58495d6f8312e6d8bbc611
Jamie Falconer <mrfluffyoven@gmail.com>
2025-11-21 11:52:42 +0000

    Fix mount options in mount_efs.sh

--- recovery/root/vendor/bin/mount_efs.sh
+++ recovery/root/vendor/bin/mount_efs.sh
@@ -1,3 +1,3 @@
 #!/sbin/sh
-mount -t ext4 -o noatime,nosuid,nodev,noauto_da_alloc,discard,journal_checksum,data=ordered,errors=panic /dev/block/bootdevice/by-name/efs /mnt/vendor/efs
-mount -t ext4 -o noatime,nosuid,nodev,noauto_da_alloc,discard,journal_checksum,data=ordered,errors=panic /dev/block/bootdevice/by-name/sec_efs /efs
+mount -t ext4 -o /dev/block/bootdevice/by-name/efs /mnt/vendor/efs
+mount -t ext4 -o /dev/block/bootdevice/by-name/sec_efs /efs
