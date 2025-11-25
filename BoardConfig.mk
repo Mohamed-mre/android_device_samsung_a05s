362d67ae162814001ab9e466e293f3ed6d9ed94b
ravindu644 <droidcasts@protonmail.com>
2025-11-25 22:44:50 +0530

    tree: re-wired vendor_boot LKMs, DLKM touch modules and pre-built kernel
    Powered by: https://github.com/ravindu644/LKM_Tools
    
    Kernel: updated all the LKMs and pre-built kernel to latest changes.
    
    2025.11.27
    
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,9 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
+# Kernel modules
+TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules)\")
+
 BOARD_ROOT_EXTRA_FOLDERS := \
     carrier \
     efs \
